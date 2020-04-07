fe0c5c6a413be49e3772fb31d29f3e371adb53c7
Exord <wecatorz@gmail.com>
2020-04-07 19:07:17 +0300

    add some try-except

--- Findsomeguys.py
+++ Findsomeguys.py
@@ -6,11 +6,9 @@ import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup as bs
+#headers = {'accept': '*/*',
 
-
-
-headers = {'accept': '*/*',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
+#           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
 
 publication_Links = []
 
@@ -36,52 +34,61 @@ def create_Publication_Link_List(shortcodes):
 
 def selenium_Launch(link):
     #ua = dict(DesiredCapabilities.CHROME)
-    #options = webdriver.ChromeOptions()
-    #options.add_argument('headless')
-    #options.add_argument('window-size=1920x935')chrome_options=options
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x935')
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get(link)
     driver.find_element_by_xpath('''//*[@id="react-root"]/section/main/div/div/article/header/div[2]/div[1]/div[1]/a''').click()
-    page_Info = driver.page_source
-    time.sleep(3)
-    #driver.close()
-    return page_Info
+    time.sleep(2.5)
+    data_List = [driver.current_url, driver.page_source]
+    #profile_Link = driver.current_url
+    #page_Info = driver.page_source
+    driver.close()
+    return data_List
 
-def get_Profile_Links(plinks, headers):
+def get_Data(plinks):
     for url in plinks:
-        soup = bs(selenium_Launch(url), 'lxml')
-
-        NameDiv = soup.find(class_="nZSzR")
-        NameTag = NameDiv.find('h2', attrs={'class': '_7UhW9 fKFbl yUEEX KV-D4 fDxYl'}).text
-        SubscribersDiv = soup.find("li", class_="Y8-fY").nextSibling.a.span.string
-        divDiscrp = soup.find(class_="-vDIg")
+        input_List = selenium_Launch(url)
+        soup = bs(input_List[1], 'lxml')
         try:
-            Discrp = divDiscrp.find("h1", class_="rhpdm").text
-            Discrp = f'''{Discrp}\n{divDiscrp.find("span").text}'''
+            NameDiv = soup.find(class_="nZSzR")
+            try:
+                NameTag = NameDiv.find('h2', attrs={'class': '_7UhW9 fKFbl yUEEX KV-D4 fDxYl'}).text
+            except:
+                NameTag = ""
+                pass
+            try:
+                SubscribersDiv = soup.find("li", class_="Y8-fY").nextSibling.a.span.string
+            except:
+                SubscribersDiv = ""
+                pass
+            try:
+                divDiscrp = soup.find(class_="-vDIg")
+            except:
+                divDiscrp = ""
+                pass
+            try:
+                Discrp = divDiscrp.find("h1", class_="rhpdm").text
+            except:
+                Discrp = ""
+                pass
+            try:
+                Discrp = f'''{Discrp}\n{divDiscrp.find("span").text}'''
+            except:
+                Discrp = ""
+                pass
         except:
-            pass
+            print('\nError ??\n')
+            next()
+            
 
-        print(NameDiv)
+        #print(NameDiv)
         print(NameTag)
         print(SubscribersDiv)
         print(Discrp)
+        print(input_List[0])
 
-
-
-        #session = requests.Session()
-        #request = session.get(url, headers = headers)
-        #if request.status_code == 200:
-
-
-            #print(f'\n | {url} - status 200 OK |')
-            #soup = bs(request.content, 'lxml')
-            #div_Profile_Link = soup.find(class_ = "e1e1d")
-            #profile_Link = soup.find('a', attrs = {'class' : "sqdOP yWX7d     _8A5w5   ZIAjV "})
-            #print(soup)
-            #profile_Link = soup.find('div', {'class': 'e1e1d'}).find('a').get('href')
-            #print(profile_Link)
-        #else:
-        #    print('error')
 
 
 
@@ -92,7 +99,9 @@ if __name__ == "__main__":
     # Entering hashtag here -
     create_Publication_Link_List(get_Hashtag_Publication_Shortcodes(input('hashtag : ')))
     print(publication_Links)
-    get_Profile_Links(publication_Links, headers)
+    get_Data(publication_Links)
+
+    print("\n ======= END =======")
 
 
 
